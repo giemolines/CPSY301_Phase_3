f7fde3980f8310825b4ff98da34699c05a9e2eaa
cam_sanjuan <135734842+giemolines@users.noreply.github.com>
2024-07-31 14:40:07 -0600

    update footer.js

--- phase_3/src/app/components/footer.js
+++ phase_3/src/app/components/footer.js
@@ -1,7 +1,7 @@
 export default function PageFooter() {
 
     return(
-        <main className="bg-black text-left text-gray-500">
+        <main className="bg-black text-left text-white">
             <div className="p-8">
                 <h2 className="mb-5">Ingraph Ltd., Calgary</h2>
                 <p className="text-xs font-light"> Ingraph is one of the leading design, signs, wraps, and printing company in Calgary, Alberta. With our industry experience and commitment, we’re confident that we can meet your business needs with our wide range of services and expertise.</p>
